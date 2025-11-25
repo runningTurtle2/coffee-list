d210ff8be72981c7683588b4a1b1e7f6557f786c
runningTurtle2 <118147333+runningTurtle2@users.noreply.github.com>
2025-11-24 23:59:14 -0800

    interface

---
+++ lib/data/data.ts
@@ -0,0 +1,15 @@
+import { Coffee } from "@/types"
+
+export const coffees:Coffee[] = [
+    {
+        id: 1,
+        name: 'Death Wish Coffee',
+        uri: '',
+        roast: 'Medium',
+        acidity: 'High',
+        process: 'Honey Roasted',
+        description: 'Sint et pariatur fugiat consectetur exercitation duis irure. Ea elit fugiat cupidatat enim Lorem incididunt veniam.',
+        tags: ['Bold', 'Honey']
+
+    }
+]
---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+export interface Coffee {
+    id:number
+    name:string 
+    uri?:string
+    roast:string
+    acidity:string
+    process:string
+    description:string
+    tags?:string[]
+}
