31ec92df830128e3eeb0f0a78fed9c35949648b1
runningTurtle2 <118147333+runningTurtle2@users.noreply.github.com>
2025-11-26 15:18:37 -0800

    form requirements set

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,32 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, } from 'react'
+import { useRouter } from "next/navigation";
 
 export default function ContactPage() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [comments, setComments] = useState('');
+  const router = useRouter();
+  let emptyValue = '';
 
   const submitForm = (e: any) => {
     e.preventDefault() // avoid page reloading // doing a GET request
-    console.log('Submit pushed')
+    console.log('Submit pushed');
     console.log(
       ' name: ' + name, '\n',
       'email: ' + email, '\n',
       'subject: ' + subject, '\n',
       'comments: ' + comments, '\n'
-    )
+    );
+    
+    router.push("/")
   }
 
+  
+
+
+// name needs to have a set char length <= 30, letters only,
 
 
   return (
@@ -37,12 +46,15 @@ export default function ContactPage() {
           <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
 
             <div className=''>
-              <label className='block text-sm font-medium'>First Name</label>
+              
+              <label className='block text-sm font-medium'>Name <span className='text-red-500'>*</span></label>
               <input
                 type='text'
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 placeholder='John'
+                required
+                maxLength={30}
                 className='mt-2 block outline-1 w-full rounded-md px-3 py-1.5 text-base focus:outline-2 focus:outline-blue-500'
               /> 
             </div>
@@ -62,10 +74,12 @@ export default function ContactPage() {
           </div>
 
           <div>
-            <label className='block text-sm font-medium'>Subject</label>
+            <label className='block text-sm font-medium'>Subject <span className='text-red-500'>*</span></label>
             <input
               type='text'
               value={subject}
+              maxLength={30}
+              required
               onChange={(e) => setSubject(e.target.value)}
               className='mt-2 block outline-1 w-full rounded-md px-3 py-1.5 text-base focus:outline-2 focus:outline-blue-500'
             />
@@ -73,15 +87,17 @@ export default function ContactPage() {
 
           <div>
             <div>
-              <label className='block text-sm font-medium'>Comments</label>
+              <label className='block text-sm font-medium'>Comments <span className='text-red-500'>*</span></label>
               <textarea
                 name='comments'
                 value={comments}
+                required
                 onChange={(e) => setComments(e.target.value)}
                 placeholder='Write here..'
                 className='mt-2 block outline-1 w-full rounded-md px-3 py-1.5 text-base focus:outline-2 focus:outline-blue-500'
               /> 
             </div>
+            <p className="text-sm font-light text-gray-400">{comments.length} of 255 max characters</p>
           </div>
 
           {/* Submit Button*/}
